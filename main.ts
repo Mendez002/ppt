fb6809140eb5f198b423cf6aab1ff28a4a2c4749
Christian Mendez <112632376+Mendez002@users.noreply.github.com>
2022-11-03 11:03:30 -0300

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,21 @@
+function Resultado () {
+    if (Valor == Azar) {
+        basic.showString("EMPATE")
+    } else if (Valor == 1 && Azar == 3) {
+        basic.showString("GANASTE")
+    } else if (Valor == 2 && Azar == 1) {
+        basic.showString("GANASTE")
+    } else if (Valor == 3 && Azar == 2) {
+        basic.showString("GANASTE")
+    } else {
+        basic.showString("PERDISTE")
+    }
+}
 input.onButtonPressed(Button.A, function () {
+    Valor += 1
+    if (Valor > 3) {
+        Valor = 1
+    }
     if (Valor == 1) {
         basic.showIcon(IconNames.Target)
     } else if (Valor == 2) {
@@ -6,13 +23,15 @@ input.onButtonPressed(Button.A, function () {
     } else {
         basic.showIcon(IconNames.Scissors)
     }
-    Valor += 1
-    if (Valor > 3) {
-        Valor = 1
-    }
 })
-function J2 () {
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 2; index++) {
+        basic.showIcon(IconNames.Target)
+        basic.showIcon(IconNames.Square)
+        basic.showIcon(IconNames.Scissors)
+    }
     Azar = randint(1, 3)
+    basic.showNumber(Azar)
     if (Azar == 1) {
         basic.showIcon(IconNames.Target)
     } else if (Azar == 2) {
@@ -20,7 +39,9 @@ function J2 () {
     } else if (Azar == 3) {
         basic.showIcon(IconNames.Scissors)
     }
-}
+    basic.clearScreen()
+    Resultado()
+})
 let Azar = 0
 let Valor = 0
-Valor = 1
+Valor = 0
